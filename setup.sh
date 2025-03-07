cf9644111df48165d4cea4b28ade5df12ee764c8
FireBirdHP <sashaban2011@outlook.com>
2025-03-07 15:53:29 +0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo "Script By FireBirdHP"
+echo " "
+echo " "
+while true; do
+    # Выводим меню
+    echo "1. Установить"
+    echo "2. Выход"
+    
+    # Читаем выбор пользователя
+    read -p ">  " choice
+    
+    # Обрабатываем выбор
+    case $choice in
+        1)
+echo "Создание папки"
+mkdir proxy
+echo "Скачивание файлов"
+wget https://raw.githubusercontent.com/FireBirdHP/fast-proxy-setup/refs/heads/main/config.json
+wget https://github.com/FireBirdHP/fast-proxy-setup/raw/refs/heads/main/mcproxy
+echo "Применение атрибута"
+chmod +x mcproxy
+echo "Запуск"
+./mcproxy
+            ;;
+        2)
+            echo "Выход..."
+            break
+            ;;
+        *)
+            echo "Ты еблан ? 1 или 2 другово не дано."
+            ;;
+    esac
+done
